56d758c561e48494fd338cf1de7aa8a8a7d1bbdb
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2023-05-05 13:43:38 +0200

    Renommage, getter, setter, méthode
    Poursuite de renommage des attributs (pour correspondre a l'uml)
    
    Création des getter et setter sur le nombres de cartes jouées
    
    Création de la méthode isClaimable

--- Shotten_Totten/Tuile.h
+++ Shotten_Totten/Tuile.h
@@ -17,11 +17,11 @@ private:
 
 	vector<Carte_c> cartesC_j1;
 	vector<Carte_t> cartesT_j1;
-	int nbCartesJoue_J1;
+	int nbCartes_J1;
 
 	vector<Carte_c> cartesC_j2;
 	vector<Carte_t> cartesT_j2;
-	int nbCarteJoues_J2;
+	int nbCartes_J2;
 
 	vector<int* []> hist; //pas sur pour le tableau de pointeur sur des int pour l'historique mais à changer
 
@@ -34,8 +34,8 @@ public:
 		this->nbCarteMax = nb_cartes;
 		this->victoirePossible = vector<Combinaison>(vicPoss);
 
-		this->nbCarteJoues_J2 = 0;
-		this->nbCartesJoue_J1 = 0;
+		this->nbCartes_J2 = 0;
+		this->nbCartes_J1 = 0;
 		
 		this->claim = 0;
 
@@ -47,6 +47,48 @@ public:
 
 	};
 
+	void ajout_c(Carte_c c, vector<Carte_c> cartes){
+
+
+	}
+
+
+	bool isClaimable() {
+		int nb_c1, nb_c2, nb_cMax;
+
+		bool claimable = false;
+
+		nb_c1 = this->getNbCartes_J1();
+		nb_c2 = this->getNbCartes_J2();
+		nb_cMax = this->getNbCartesMax();
+
+		if (nb_c1 == nb_cMax || nb_c2 == nb_cMax) {
+			if (abs(nb_c1 - nb_c2) <= 1)
+				claimable = true;
+		}
+
+		return claimable;
+	}
+
+	int getNbCartes_J1() {
+		return this->nbCartes_J1;
+	}
+
+	int getNbCartes_J2() {
+		return this->nbCartes_J2;
+	}
+
+	int getNbCartesMax() {
+		return this->nbCarteMax;
+	}
+
+	void setNbCartes_J1(int newNb){
+		this->nbCartes_J1 = newNb;
+	}
+
+	void setNbCartes_J2(int newNb) {
+		this->nbCartes_J2 = newNb;
+	}
 };
 
 #endif // !TUILE_H
