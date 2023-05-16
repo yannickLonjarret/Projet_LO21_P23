c486c7639b1f0be62fab75c6ae82daa6705ba481
remibchrd <133136188+remibchrd@users.noreply.github.com>
2023-05-16 18:27:59 +0200

    Ajout classes CarteTactique
    Ajout des différentes classes pour les cartes tactiques : CarteTactique, ModeCombat, Ruse, TroupeElite

--- Shotten_Totten/Carte_t.cpp
+++ Shotten_Totten/Carte_t.cpp
@@ -1,4 +1,17 @@
 #include "Carte_t.h"
+#include "Carte_c.h"
+using namespace std;
 
+Carte_c& TroupeElite::definir_carte(int v, const string& c){
+    if(v < val_deb || v > val_fin)
+        throw CarteTactiqueException("Error : value out of range");
+    Carte_c carte = Carte_c(v, c);
+    return carte; 
+}
+
+ostream& operator<<(ostream& f, const TroupeElite& c){
+    f << "Nom : " << c.getNom() << "\nDébut : " << c.getDebut() << "\nFin : " << c.getFin();
+    return f;
+}
 
 //tydfyugh
