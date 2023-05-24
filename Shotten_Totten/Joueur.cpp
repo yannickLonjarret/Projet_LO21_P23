bf8bc83762e5def747c87f65cbc14678452947ec
Jules MONDESERT <jules.mondesert@etu.utc.fr>
2023-05-24 14:15:14 +0200

    correction et implementation de Pioche_c() et Poser_carte_c()

---
+++ Shotten_Totten/Joueur.cpp
@@ -0,0 +1,31 @@
+#include "Joueur.h"
+
+void Poser_carte_c(Carte_c c,id_joueur id, Jeu& jeu){
+    //Logique de pose d'une carte classique sur le plateau
+    bool carteTrouvee = false;
+    for (auto it = cartes_c.begin(); it != cartes_c.end();++it){
+        if (it == c){
+            carteTrouvee = true;
+            cartes_c.erase(it);
+            setNb_cartes();
+            break;
+        }
+    }
+    if (carteTrouve){
+        jeu.Poser_carte_c(c,id);
+    }else {
+        std::cout << "La carte n'appartient pas à la main du joueur !" << std::endl;
+    }
+}
+
+Carte_c Pioche_c(Jeu& jeu){
+    //Logique de la pioche d'une carte classique
+    Carte_c carte;
+    if (!jeu.getPioche_c().estVide()) {
+        carte = jeu.getPioche_c().piocher();
+        cartes_c.push_back(carte);
+        setNb_cartes();
+    }
+    return carte;
+}
+
--- Shotten_Totten/Joueur.h
+++ Shotten_Totten/Joueur.h
@@ -6,8 +6,6 @@
 #include <vector>
 #include <ostream>
 #include "Jeu.h"
-#include "Carte_c.h"
-#include "Carte_t.h"
 
 /// <summary>
 /// Joueur represents the players.
@@ -15,7 +13,7 @@
 
 class Joueur
 {
-private:
+protected:
 
 	string nom;
 	//test pour donner un identifiant unique
@@ -37,7 +35,7 @@ public:
 	/// <param name="cartes_c"> optionnal, player classic card vector : vector<cartes_c> </param>
 	/// <param name="cartes_t"> optionnal, player tactic card vector : vector<cartes_t> </param>
 
-	Joueur(string const& n) : nom(n)
+	Joueur(string const &n) : nom(n)
 	{
 		id_joueur = prochain_id; //Attribuer l'id unique a l'instance actuelle
 		prochain_id++;//Incrémenter l'id unique pour la prochaine instance
@@ -45,11 +43,11 @@ public:
 		score = 0;
 	}
 
-	int setNb_cartes() {
-		nb_cartes = cartes_c.size() + cartes_t.size();
+	int setNb_cartes(){
+		nb_cartes= cartes_c.size() + carte_t.size();
 	}
 
-	int setScore(int s) {
+	int setScore(int s){
 		score += s;
 	}
 	/// <summary>
@@ -58,55 +56,39 @@ public:
 
 	~Joueur() = default;
 
-	friend std::ostream& operator<<(std::ostream& os, const Joueur& j);
+	friend std::ostream &operator<<(std::ostream &os, const Joueur &j);
 
-	string getNom() const
+	const std::string getNom()
 	{
 		return this->nom;
 	}
 
-	int getNbCartes() const
+	const int getNbCartes()
 	{
 		return this->nb_cartes;
 	}
 
-	int getScore() const
+	const int getScore()
 	{
 		return this->score;
 	}
 
-	std::vector<Carte_c> getCarteC() const
+	const std::vector<Carte_c> getCarteC()
 	{
 		return this->cartes_c;
 	}
 
-	std::vector<Carte_t> getCarteT() const
+	const std::vector<Carte_t> getCarteT()
 	{
 		return this->cartes_t;
 	}
 
-	Carte_c Pioche_c()
-	{
-		if (Pioche_c::getSize() == 0)
-		{
-		}
-		else
-		{
-		}
-	}
+	Carte_c Pioche_c(Jeu& jeu);
 
-	Carte_t Pioche_t()
-	{
-		if (Pioche_t::getSize() == 0)
-		{
-		}
-		else
-		{
-		}
-	}
+	Carte_t Pioche_t();
 
-	void Poser_carte_c(Carte_c c, int id);
-	void Poser_carte_t(Carte_t c, int id);
+	void Poser_carte_c(Carte_c c,id_joueur id, Jeu& jeu);
+	void Poser_carte_t(Carte_t c,id_joueur id);
 	void surrender();
 	void to_claim();
 	void look_graveyard();
@@ -120,25 +102,13 @@ int Joueur::prochain_id = 0; // Initialisation de la variable statique
 /// <param name="os"> the output stream</param>
 /// <param name="j"> the player to display</param>
 /// <returns> the output stream with the card's information</returns>
-std::ostream& operator<<(std::ostream& os, const Joueur& j)
+std::ostream &operator<<(std::ostream &os, const Joueur &j)
 {
 	os << "Joueur : \n\t Name : " << j.getNom() << "\n\t "
-		<< "Nombre cartes : " << j.getNbCartes() << "\n\t "
-		<< "Score : " << j.getScore() << "\n\t ";
-	os << "Main : Carte_c[";
-	for (int i = 0; i < j.getCarteC().size(); i++) {
-		os << j.getCarteC()[i];
-		if (i != j.getCarteC().size() - 1)
-			os << ", ";
-	}
-	os << "]\n";
-	os << "Main : Carte_t[";
-	for (int i = 0; i < j.getCarteT().size(); i++) {
-		os << j.getCarteT()[i];
-		if (i != j.getCarteT().size() - 1)
-			os << ", ";
-	}
-	os << "]\n" << std::endl;
+	   << "Nombre cartes : " << j.getNbCartes() << "\n\t "
+	   << "Score : " << j.getScore() << "\n\t "
+	   << "Main : " << j.getCarteC() << j.getCarteT() << "\n\t "
+	   << std::endl;
 	return os;
 }
 
