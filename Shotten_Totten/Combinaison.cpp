8ad8a032877f899f4e01bdcaab9a9eeb538c7b9a
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2023-06-09 09:47:28 +0200

    separatoin .h/.cpp combinaison

--- Shotten_Totten/Combinaison.cpp
+++ Shotten_Totten/Combinaison.cpp
@@ -1,1 +1,137 @@
 #include "Combinaison.h"
+
+bool Combinaison::isSuite(vector<Carte_c*> combi) {
+	bool res = true;
+
+	size_t i = 1;
+
+	while (res && i < getTailleCombi()) {
+
+		if (combi[i - 1]->getValeur() != combi[i]->getValeur() - 1)
+			res = false;
+
+		i++;
+	}
+
+	return res;
+}
+
+bool Combinaison::isBrelan(vector<Carte_c*> combi) {
+	bool res = true;
+
+	size_t i = 1;
+
+	while (res && i < getTailleCombi()) {
+
+		if (combi[i - 1]->getValeur() != combi[i]->getValeur())
+			res = false;
+
+		i++;
+	}
+
+	return res;
+}
+
+bool Combinaison::isCouleur(vector<Carte_c*> combi) {
+	bool res = true;
+
+	size_t i = 1;
+
+	while (res && i < getTailleCombi()) {
+
+		if (combi[i - 1]->getCouleur() != combi[i]->getCouleur())
+			res = false;
+
+		i++;
+	}
+
+	return res;
+
+}
+
+int Combinaison::calculSumCombi(vector<Carte_c*> combi) {
+	int s = 0;
+
+	for (size_t i = 0; i < getTailleCombi(); i++) {
+		s += combi[i]->getValeur();
+	}
+	return s;
+}
+
+int Combinaison::calculScoreCombi(Combinaison* c) {
+	if (c->getCouleur() && c->getSuite())
+		return 5;
+
+	if (c->getBrelan())
+		return 4;
+
+	if (c->getCouleur())
+		return 3;
+
+	if (c->getSuite())
+		return 2;
+
+	return 1;
+}
+
+void Combinaison::dropDown(vector<Combinaison*> lstCombi) {
+	switch (getScoreCombi())
+	{
+	case 5:
+		if (isCombiInLst(3, lstCombi)) {
+			setDefault();
+			convertToCouleur();
+		}
+		else if (isCombiInLst(2, lstCombi)) {
+			setDefault();
+			convertToSuite();
+		}
+		else {
+			setDefault();
+			convertToSomme();
+		}
+		return;
+
+	case 4:
+		if (isCombiInLst(3, lstCombi) && getCouleur()) {
+			setDefault();
+			convertToCouleur();
+		}
+		else {
+			setDefault();
+			convertToSomme();
+		}
+		return;
+
+	case 3:
+		setDefault();
+		convertToSomme();
+
+		return;
+
+	case 2:
+		setDefault();
+		convertToSomme();
+
+		return;
+		break;
+	case 1:
+		//Do nothing
+		break;
+	default:
+		cout << "Erreur Dropdown combi: score inexistant" << endl;
+		break;
+	}
+
+	return;
+}
+
+
+bool Combinaison::isCombiInLst(int scoreCombi, vector<Combinaison*> lstCombi) {
+	for (int i = 0; i < lstCombi.size(); i++) {
+		if (lstCombi[i]->getScoreCombi() == scoreCombi)
+			return true;
+	}
+
+	return false;
+}
--- Shotten_Totten/Combinaison.h
+++ Shotten_Totten/Combinaison.h
@@ -40,141 +40,18 @@ public:
 		scoreCombi = calculScoreCombi(this);
 	}
 
-	bool isSuite(vector<Carte_c*> combi) {
-		bool res = true;
+	bool isSuite(vector<Carte_c*> combi);
 
-		size_t i = 1;
+	bool isBrelan(vector<Carte_c*> combi);
 
-		while (res && i < getTailleCombi()) {
+	bool isCouleur(vector<Carte_c*> combi);
 
-			if (combi[i - 1]->getValeur() != combi[i]->getValeur() - 1)
-				res = false;
+	int calculSumCombi(vector<Carte_c*> combi);
 
-			i++;
-		}
+	int calculScoreCombi(Combinaison* c);
 
-		return res;
-	}
-
-	bool isBrelan(vector<Carte_c*> combi) {
-		bool res = true;
-
-		size_t i = 1;
-
-		while (res && i < getTailleCombi()) {
-
-			if (combi[i - 1]->getValeur() != combi[i]->getValeur())
-				res = false;
-
-			i++;
-		}
-
-		return res;
-	}
-
-	bool isCouleur(vector<Carte_c*> combi) {
-		bool res = true;
-
-		size_t i = 1;
-
-		while (res && i < getTailleCombi()) {
-
-			if (combi[i - 1]->getCouleur() != combi[i]->getCouleur())
-				res = false;
-
-			i++;
-		}
-
-		return res;
-
-	}
-
-	int calculSumCombi(vector<Carte_c*> combi) {
-		int s = 0;
-
-		for (size_t i = 0; i < getTailleCombi(); i++) {
-			s += combi[i]->getValeur();
-		}
-		return s;
-	}
-
-	int calculScoreCombi(Combinaison* c) {
-		if (c->getCouleur() && c->getSuite())
-			return 5;
-
-		if (c->getBrelan())
-			return 4;
-
-		if (c->getCouleur())
-			return 3;
-
-		if (c->getSuite())
-			return 2;
-
-		return 1;
-	}
-
-	void dropDown(vector<Combinaison*> lstCombi) {
-		switch (getScoreCombi())
-		{
-		case 5:
-			if (isCombiInLst(3, lstCombi)) {
-				setDefault();
-				convertToCouleur();
-			}
-			else if (isCombiInLst(2, lstCombi)) {
-				setDefault();
-				convertToSuite();
-			}
-			else {
-				setDefault();
-				convertToSomme();
-			}
-			return;
-
-		case 4:
-			if (isCombiInLst(3, lstCombi) && getCouleur()) {
-				setDefault();
-				convertToCouleur();
-			}
-			else {
-				setDefault();
-				convertToSomme();
-			}
-			return;
-
-		case 3:
-			setDefault();
-			convertToSomme();
-
-			return;
-
-		case 2:
-			setDefault();
-			convertToSomme();
-
-			return;
-			break;
-		case 1:
-			//Do nothing
-			break;
-		default:
-			cout << "Erreur Dropdown combi: score inexistant" << endl;
-			break;
-		}
-
-		return;
-	}
-
-
-	bool isCombiInLst(int scoreCombi, vector<Combinaison*> lstCombi) {
-		for (int i = 0; i < lstCombi.size(); i++) {
-			if (lstCombi[i]->getScoreCombi() == scoreCombi)
-				return true;
-		}
-
-		return false;
-	}
+	void dropDown(vector<Combinaison*> lstCombi);
+	bool isCombiInLst(int scoreCombi, vector<Combinaison*> lstCombi);
 
 	void convertToCouleur() {
 		setCouleur(true);
