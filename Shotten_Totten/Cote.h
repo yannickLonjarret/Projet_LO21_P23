37f843f6ab27753b418b1479fff90cf1a5a5d8d1
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2023-06-12 17:27:36 +0200

    Documentation Classe cote

--- Shotten_Totten/Cote.h
+++ Shotten_Totten/Cote.h
@@ -1,12 +1,30 @@
 #pragma once
+
+/// <summary>
+/// Classe cote permettant de standardiser la gestion des cartes sur une tuile.
+/// </summary>
 class Cote
 {
 
 private:
-	int idJoueur; //indice du vecteur du joueur
+	/// <summary>
+	/// Indice du joueur dans le vecteur de cote dans la classe tuile
+	/// </summary>
+	int idJoueur;
+
+	/// <summary>
+	/// Nombre de cartes classique jouée par le joueur
+	/// </summary>
 	int nbCartesJoue;
 
+	/// <summary>
+	/// Ensemble des cartes classiques présente du côté d'un joueur sur la tuile
+	/// </summary>
 	vector<Carte_c*> cartesC;
+
+	/// <summary>
+	/// Ensemble des cartes classiques présente du côté d'un joueur sur la tuile
+	/// </summary>
 	vector<Carte_t*> cartesT;
 
 public:
@@ -22,19 +40,35 @@ public:
 		return nbCartesJoue;
 	}
 
+	/// <summary>
+	/// Met à jour le nombre de carte classique jouée sur le côté d'un joueur
+	/// </summary>
+	/// <param name="newVal">Nouveau nombre de carte classique présent sur une tuile</param>
 	void setNbCartes(int newVal) {
 		nbCartesJoue = newVal;
 	}
 
+	/// <summary>
+	///
+	/// </summary>
+	/// <returns></returns>
 	int getIdJoueur() const {
 		return idJoueur;
 	}
 
+	/// <summary>
+	/// Retourne l'ensemble des cartes classiques jouée sur le côté d'un joueur
+	/// </summary>
+	/// <returns>Une référence d'un vecteur pour pouvoir enregistrer les modifications</returns>
 	vector<Carte_c*>& getCartesC() {
 
 		return cartesC;
 	}
 
+	/// <summary>
+	/// Retourne l'ensemble des cartes tactiques jouée sur le côté d'un joueur
+	/// </summary>
+	/// <returns>Une référence d'un vecteur pour pouvoir enregistrer les modifications</returns>
 	vector<Carte_t*>& getCartesT() {
 
 		return cartesT;
